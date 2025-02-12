7ff2eae9bbc52cf0436ca92cda5a4ecd7b69b88f
Fiar Labs <fiarlabs@fiars-mbp.lan>
2025-02-11 17:08:06 -0800

    update condig

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    'tailwindcss': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
+    }
+  }
 } 
