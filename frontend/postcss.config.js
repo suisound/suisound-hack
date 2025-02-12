be70750278f3a43033b7ef9a6925cbacd0f4291d
Fiar Labs <fiarlabs@fiars-mbp.lan>
2025-02-11 17:28:40 -0800

    sui

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
+    'postcss-import': {},
+    'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
       features: {
+        'custom-properties': false,
         'nesting-rules': false
       }
-    }
+    },
+    // Disable minification
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          'cssnano': false
+        }
+      : {})
   }
 } 
